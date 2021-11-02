5553eaee1562d8cabf841c75a403bd2c68fa5350
Maxim Shirokostup <maximshirokostup@capdefier.com>
2021-11-02 17:05:10 +0200

    Fix logic of load all ingredients

--- lib/ui/pages/home_page/view/home_page.dart
+++ lib/ui/pages/home_page/view/home_page.dart
@@ -10,7 +10,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    context.read<IngredientCubit>().loadAllIngredients();
     final TextFieldLoaderWidget _textFieldLoaderWidget = TextFieldLoaderWidget();
 
     return BlocProvider(
